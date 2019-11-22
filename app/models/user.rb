6d817c0104d0b99baecf7670b35a3ba9c66a17c1
kamioka1230 <hiphop.pizzaboi@gmail.com>
2019-11-22 15:36:26 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
@@ -11,6 +12,10 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
 end
